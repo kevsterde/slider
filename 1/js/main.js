0fe73a1b560b7c91f9b87eabbe77870759e106c1
SENSlE <96121161+SENSlE@users.noreply.github.com>
2023-05-26 23:24:29 +0800

    sample

---
+++ 1/js/main.js
@@ -0,0 +1,25 @@
+
+//Swiper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+  loop: true,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+
+
+  
+});
